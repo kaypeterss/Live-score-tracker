777d9f3375f13777a478df89a7b177a1b8e4035b
Kay Peters <kay.peters@gmail.com>
2026-09-19 17:40:23 +0200

    Fix OBS pre-game turn label

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='threshold-tactics-shell-v1';
+const CACHE_NAME='threshold-tactics-shell-v2';
 const SHELL=['./','./manifest.webmanifest','./threshold-logo.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('threshold-tactics-')&&key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
